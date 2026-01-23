d0ee4d3b1fff223946ac2be4d33d566c9f9907e3
Amin Mohamed <99421780+aminmohaamed@users.noreply.github.com>
2026-01-23 19:41:18 +0200

    center text on view details button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,11 +6,11 @@ function Button({ children, disabled, to, type, onClick }) {
   const styles = {
     primary:
       base +
-      " w-full px-3 py-1 sm:px-6 sm:py-4 bg-green-600 hover:bg-green-700 hover:text-white focus:ring-green-400 disabled:bg-green-300 active:bg-green-800  disabled:text-green-100 disabled:hover:bg-green-300",
+      " w-full px-3 py-1 sm:px-6 sm:py-4 bg-green-600 hover:bg-green-700 hover:text-white focus:ring-green-400 disabled:bg-green-300 active:bg-green-800  disabled:text-green-100 disabled:hover:bg-green-300 ",
     small:
       base +
       " px-4 py-2 sm:px-5 sm:py-2.5 text-xs bg-green-600 hover:bg-green-700 hover:text-white focus:ring-green-400 disabled:bg-green-300 active:bg-green-800  disabled:text-green-100 disabled:hover:bg-green-300",
-    black: base + "bg-black text-white px-4 py-3 rounded w-full",
+    black: base + "bg-black text-white px-4 py-3 rounded w-full text-center",
     secoundray:
       "duration-3 text-sm inline-block rounded-full  border-2 border-stone-400 font-semibold uppercase tracking-wide text-stone-400 transition-colors hover:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-200 focus:text-stone-800 focus:ring-offset-2 disabled:cursor-not-allowed px-3 py-2.5 sm:px-6 sm:py-4 md:py-3.5 hover:text-stone-800",
     round:
